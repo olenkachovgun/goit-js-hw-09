2b0918e813a82ce54a03cc33785e9d18a75ff309
olenkachovgun <59543079+olenkachovgun@users.noreply.github.com>
2024-11-17 14:31:57 +0000

    Deploying to gh-pages from @ olenkachovgun/goit-js-hw-09@a6c79c2bcbe9ccd558c5495ff633164b5a39988e 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/styles-BfkkNdGt.js";console.log("Form");
+import"./assets/styles-Iu3u4Bx4.js";const e=document.querySelector("#email-input");e.addEventListener("focus",()=>{e.setAttribute("placeholder","Type area")});e.addEventListener("blur",()=>{e.setAttribute("placeholder","")});
 //# sourceMappingURL=2-form.js.map
